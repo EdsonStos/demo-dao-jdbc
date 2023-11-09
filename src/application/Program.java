b9134d7a61b7a3deec69430146a4535974ce3dbc
Edson Santos <edson_jjsantos@hotmail.com>
2023-11-09 18:22:04 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,8 @@
 package application;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 //import java.sql.Connection;
 //import java.sql.PreparedStatement;
 //import java.sql.SQLException;
@@ -10,7 +13,10 @@ public class Program {
 	public static void main(String[] args) {
 		
 			Department obj = new Department(1, "Books");
-			System.out.println(obj);
+			
+			Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+			
+			System.out.println(seller);
 	}
 }			
 			//Connection conn = null;
